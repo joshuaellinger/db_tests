43b18d3a5566317239498443ce8461430aafeba8
Joshua Ellinger <josh@exemplartech.com>
2020-04-12 01:51:51 -0500

    based database wrapper

--- shared/database.py
+++ shared/database.py
@@ -1,11 +1,234 @@
 from loguru import logger
-from typing import Dict
+from typing import Dict, List, Tuple
 from configparser import ConfigParser
 import os
 import psycopg2
 
-def config(filename='database.ini', section='postgresql') -> Dict:
+from datetime import date, datetime
+import pandas as pd
+import numpy as np
 
+class Database:
+
+    def __init__(self, params: Dict):
+        self.conn = psycopg2.connect(**params)
+        self.cursor = None        
+        self.schema = None
+        self.dtype_map = None
+
+
+    def close(self):
+        if self.cursor != None: self.cursor.close()
+        self.cursor = None
+        self.conn.close()
+        self.conn = None
+
+
+    def execute(self, smt: str, *args, **kwargs) -> None:
+        " run a command against the DB"
+        conn = self.conn
+        try:
+            if self.cursor: raise Exception("cursor is still in-use")
+            self.cursor = conn.cursor()
+            if len(args) > 0:
+                self.cursor.execute(smt, args)
+            else:
+                self.cursor.execute(smt, kwargs)
+            conn.commit()
+        except Exception:
+            logger.error(f"execute failed: {smt}")    
+            raise Exception("execute failed")
+        finally:
+            self.cursor.close()
+            self.cursor = None
+
+
+    def _query(self, smt: str, args: Tuple, kwargs: Dict):
+        " query (generic, returns a cursor)"
+        conn = self.conn
+        try:
+            if self.cursor: raise Exception("cursor is still in-use")
+            self.cursor = conn.cursor()
+            if len(args) > 0:
+                self.cursor.execute(smt, args)
+            else:
+                self.cursor.execute(smt, kwargs)
+            return self.cursor
+        except Exception:
+            self.cursor.close()
+            self.cursor = None
+            logger.error(f"query failed: {smt}")    
+            raise Exception("query failed")
+
+    def query(self, smt: str, *args, **kwargs):
+        " query (generic, returns a cursor)"
+        return self._query(smt, args, kwargs)
+
+    def release(self):
+        if not self.cursor: raise Exception("cursor is already released")
+        self.cursor.close()
+        self.cursor = None
+
+    def query_scalar(self, smt: str, *args, **kwargs):
+        " query a single value "
+        cur = self._query(smt, args, kwargs)
+        try:
+            row = cur.fetchone()
+            if row is None: return None
+            if len(row) != 1: raise Exception("query returned too many values")
+            return row[0]
+        finally:
+            self.release()
+
+    def query_one(self, smt: str, *args, **kwargs) -> Tuple:
+        " query a single row (returns a tuple) "
+        cur = self._query(smt, args, kwargs)
+        try:
+            return cur.fetchone()
+        finally:
+            self.release()
+
+    def query_one_dict(self, smt: str, *args, **kwargs) -> Dict:
+        " query a single row (returns a dictionary) "
+        cur = self._query(smt, args, kwargs)
+        try:
+            row = cur.fetchone()
+            result = { cur.description[i][0]: v for i,v in enumerate(row) }
+            return result
+        finally:
+            self.release()
+
+    def query_many(self, smt: str, *args, **kwargs) -> List[Tuple]:
+        " query more than one row (returns a list of tuples)"
+        cur = self._query(smt, args, kwargs)
+        try:
+            return cur.fetchall()
+        finally:
+            self.release()
+
+    def query_many_dict(self, smt: str, *args, **kwargs) -> List[Dict]:
+        " query multiple rows (returns a list of dictionarys)"
+        cur = self._query(smt, args, kwargs)
+        try:
+            rows = cur.fetchmany(20)
+            if len(rows) == 0: return []
+
+            names = [i[0] for i in cur.description]        
+
+            result = []
+            while len(rows) != 0:
+                for r in rows:
+                    result.append({ names[i]: v for i,v in enumerate(r) })
+                rows = cur.fetchmany(20)
+            return result
+        finally:
+            self.release()
+
+    def _load_dtype_map(self):
+        recs = self.query_many("""
+SELECT oid, typname
+from pg_type
+where typname in (
+    'bool', 
+    'int2', 'int4', 'int8', 
+    'float4', 'float8',
+    'char', 'text', 'varchar',
+    'date')
+""")
+
+        names_to_dtypes = {
+            'bool': np.bool,
+            'int2': np.int16,
+            'int4': np.int32,
+            'int8': np.int64,
+            'float4': np.float,
+            'float8': np.double,
+            'char': np.char,
+            'text': np.str,
+            'varchar': np.str,
+            'date': date
+        }
+
+        results = {}
+        for oid, typname in recs:
+            results[oid] = names_to_dtypes[typname]
+        self.dtype_map = results            
+
+
+    def _make_array(self, nrows: int, descr) -> np.array:
+        
+        dtype = self.dtype_map.get(descr.type_code)
+        if dtype == None:
+            raise Exception(f"Column {descr.name} has an unsupported type_code ({descr.type_code}")
+        return np.zeros(nrows, dtype=dtype)
+
+    def query_frame(self, smt: str, *args, **kwargs) -> pd.DataFrame:
+        " query multiple rows (returns a data frame)"
+
+        if self.dtype_map == None: self._load_dtype_map()
+
+        cur = self._query(smt, args, kwargs)
+        try:
+
+            names = [d[0] for d in cur.description]
+            ncols = len(names)
+            vecs = [self._make_array(cur.rowcount, d) for d in cur.description]
+
+            if cur.rowcount > 0: 
+                idx = 0
+                rows = cur.fetchmany(100)
+                while len(rows) != 0:
+                    for r in rows:
+                        for i in range(ncols):
+                            vecs[i][idx] = r[i]
+                        idx += 1
+                    rows = cur.fetchmany(100)
+            return pd.DataFrame({ n:v for n,v in zip(names, vecs) })
+        finally:
+            self.release()
+
+# --------------------------------------------------
+class DatabaseSchema:
+
+    def __init__(self, db: Database):
+        self.db = db
+
+    def does_table_exist(self, table_name: str) -> bool:
+        "check if a table exists"
+
+        n = self.db.query_scalar("""
+SELECT table_name 
+FROM information_schema.tables 
+WHERE table_schema = 'public' and table_name = %s
+""", table_name)
+        return n != None
+
+    def does_column_exist(self, table_name: str, column_name) -> bool:
+        "check if a column exists on a table"
+        n = self.db.query_scalar("""
+SELECT column_name
+FROM information_schema.columns 
+WHERE table_schema = 'public' and table_name = %s
+  AND column_name = %s
+""", table_name, column_name)
+        return n != None
+
+    def get_table_schema(self, table_name: str) -> List[Dict]:
+        "get the schema of a table (column_name, ordinal_position, data_type, character_maximum_length)"
+
+        t = self.db.query_many("""
+SELECT column_name, ordinal_position, data_type, character_maximum_length 
+FROM information_schema.columns 
+WHERE table_schema = %t
+ORDER BY ordinal_postion
+""", t=table_name)
+        return t
+
+
+# ----------------------------------
+
+def config(filename='database.ini', section='postgresql') -> Dict:
+    "read the configuration values out of an .ini file"
     parser = ConfigParser()
 
     for p in [".", "..", "../.."]:
@@ -23,11 +246,14 @@ def config(filename='database.ini', section='postgresql') -> Dict:
 
     raise Exception("Could not find {0}".format(filename))
 
-def connect_to_db():
 
-    params = config()
+def connect_to_db(params: Dict = None) -> Database:
+    "get an instance of the Database wrapper"
 
-    logger.info("  connect at {0}".format({ x: params[x] for x in params if x != "password"}))
+    if params == None:
+        params = config()
 
-    conn = psycopg2.connect(**params)
-    return conn
+    logger.debug("  connect at {0}".format({ x: params[x] for x in params if x != "password"}))
+    db =  Database(params)
+    db.schema = DatabaseSchema(db)
+    return db
---
+++ tests/test_database.py
@@ -0,0 +1,115 @@
+import pytest
+from loguru import logger
+from shared.database import connect_to_db, config, Database 
+
+from psycopg2 import OperationalError
+
+@pytest.fixture
+def db():
+    db = connect_to_db()
+    if db.schema.does_table_exist("test_table"):
+        db.execute("DROP TABLE test_table")
+
+    yield connect_to_db()
+
+    #if db.schema.does_table_exist("test_table"):
+    #    db.execute("DROP TABLE test_table")
+
+def x_test_connection():
+    db = connect_to_db()
+    db.close()
+
+@pytest.mark.slow
+def test_bad_connection():
+    with pytest.raises(TypeError) as excinfo:
+        connect_to_db({})
+
+    c = config()
+    c["host"] = "xxxx"
+    with pytest.raises(OperationalError) as excinfo:
+        connect_to_db(c)
+    assert(str(excinfo.value) == 'could not translate host name "xxxx" to address: Unknown host\n')
+
+    c = config()
+    c["host"] = "covidtracking.com"
+    c["connect_timeout"] = 1
+    with pytest.raises(OperationalError) as excinfo:
+        connect_to_db(c)
+    assert(str(excinfo.value) == 'timeout expired\n')
+
+    c = config()
+    c["user"] = "xxxx"
+    with pytest.raises(OperationalError) as excinfo:
+        connect_to_db(c)
+    assert(str(excinfo.value) == 'FATAL:  password authentication failed for user "xxxx"\n')
+
+    c = config()
+    c["password"] = "xxxx"
+    with pytest.raises(OperationalError) as excinfo:
+        connect_to_db(c)
+    assert(str(excinfo.value) == 'FATAL:  password authentication failed for user "covid"\n')
+
+    c = config()
+    c["database"] = "xxxx"
+    with pytest.raises(OperationalError) as excinfo:
+        connect_to_db(c)
+    assert(str(excinfo.value) == 'FATAL:  database "xxxx" does not exist\n')
+
+
+def x_test_schema(db):
+
+    assert(not db.schema.does_table_exist("test_table"))
+
+    db.execute("""
+CREATE TABLE test_table
+(
+    col varchar(2)
+)
+""")
+
+    assert(db.schema.does_table_exist("test_table"))
+    assert(db.schema.does_column_exist("test_table", "col"))
+    assert(not db.schema.does_column_exist("test_table", "x"))
+
+
+def test_query(db):
+
+    db.execute("""
+CREATE TABLE test_table
+(
+    state varchar(2),
+    positive int
+)
+""")
+
+    db.execute("INSERT INTO test_table (state, positive) VALUES ('AL',1000)")
+    db.execute("INSERT INTO test_table (state, positive) VALUES ('TX',1001)")
+
+    n = db.query_scalar("SELECT positive FROM test_table WHERE state = 'AL'")
+    assert(n == 1000)
+    s = db.query_scalar("SELECT state from test_table where positive = 1001")
+    assert(s == "TX")  
+
+    row = db.query_one("SELECT positive FROM test_table WHERE state = 'AL'")
+    assert(len(row) == 1 and row[0] == 1000)
+
+    row = db.query_one("SELECT positive FROM test_table WHERE state = 'XX'")
+    assert(row is None)
+
+    rows = db.query_many("SELECT * FROM test_table ORDER BY state")
+    assert(len(rows) == 2 and rows[0][1] == 1000 and rows[1][1] == 1001)
+
+    rows = db.query_many("SELECT * FROM test_table WHERE positive = 0")
+    assert(len(rows) == 0)
+
+    rows = db.query_many_dict("SELECT * FROM test_table ORDER BY state")
+    assert(len(rows) == 2 and rows[0]["positive"] == 1000 and rows[1]["positive"] == 1001)
+
+    rows = db.query_many_dict("SELECT * FROM test_table where positive = 0")
+    assert(len(rows) == 0)
+
+    df = db.query_frame("SELECT * FROM test_table ORDER BY state")
+    assert(df.shape[0] == 2 and df.positive[0] == 1000 and df.positive[1] == 1001)
+
+    df = db.query_frame("SELECT * FROM test_table where positive = 0")
+    assert(df.shape[0] == 0)
